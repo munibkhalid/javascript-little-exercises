39e6f6cb4661b5f1b8c4a32e50f1eed7dd07895d
munibkhalid <53476269+munibkhalid@users.noreply.github.com>
2020-05-29 11:06:30 -0700

    Create class-practice.js
    Create a Book constructor that has two properties :
    
    Title
    Author
    and two methods:
    
    A method named getTitle that returns: "Title: " + the instance title.
    A method named getAuthor that returns: "Author: " + the instance author.
    and instantiate this constructor by creating 3 new books:
    
    Pride and Prejudice - Jane Austen (PP)
    Hamlet - William Shakespeare (H)
    War and Peace - Leo Tolstoy (WP)
    Name the new object instances PP, H, and WP, respectively.
    
    For instance, if I instantiated the following book using this Book constructor function:
    
    Harry Potter - J.K. Rowling (HP)

---
+++ class-practice.js
@@ -0,0 +1,18 @@
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  getTitle() {
+    return `Title: ${this.title}`;
+  }
+
+  getAuthor() {
+    return `Author: ${this.author}`;
+  }
+}
+
+const PP = new Book('Pride and Prejudice', 'Jane Austen');
+const H = new Book('Hamlet', 'William Shakespeare');
+const WP = new Book('War and Peace', 'Leo Tolstoy');
